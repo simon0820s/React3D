4ff527664fd03b5c08937ed6b178bab972c146b4
simon0820s <simon0820s@gmail.com>
2023-05-13 10:00:57 -0500

    add components and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import Hero from './components/Hero'
+import Who from './components/Who'
+import Works from './components/Works'
+import Contact from './components/Contact'
+
+import styled from "styled-components"
+
+const Container = styled.div`
+  height:100vh;
+  background-color:rebeccapurple;
+`
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
   )
 }
 
